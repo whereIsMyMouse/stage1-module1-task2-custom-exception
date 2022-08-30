0e8d2ffbfbbe495672fc87a42dad48a57e66cd01
shariy <sharii.margarita@gmail.com>
2022-08-31 00:59:02 +0300

    commit2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,26 +3,30 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID){
-   try {
-     return Student.getValueOf(studentID);
-   } catch (IllegalArgumentException e){
-      throw new StudentNotFoundException("Could not find student with ID " + String.valueOf(studentID));
-    }
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-
+    public Student find(long studentID) throws StudentNotFoundException {
+        boolean searchResult = false;
+        for (long ID : IDs) {
+            if (studentID == ID) {
+                searchResult = true;
+                break;
+            }
+        }
+        if(searchResult) {
+            return Student.getValueOf(studentID);
+        } else{
+            throw new StudentNotFoundException("Could not find student with ID " + studentID);
+        }
     }
 
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
 
 
-  }
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+    }
 }
--- src/main/java/com/epam/mjc/StudentNotFoundException.java
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -1,7 +1,15 @@
 package com.epam.mjc;
 
-public class StudentNotFoundException extends IllegalArgumentException{
-    public StudentNotFoundException(String message){
+public class StudentNotFoundException extends IllegalArgumentException {
+    //public String message;
+
+    public StudentNotFoundException(String message) {
         super(message);
     }
+
+//    @Override
+//    public String getMessage() {
+//        return message;
+//    }
+
 }
